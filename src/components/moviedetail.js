769cef0cfb3e9dc0a81e5dc99bb45a84f7232716
Winn Pham <wpham213@gmail.com>
2026-04-18 19:57:33 -0600

    created add review feature

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,83 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { fetchMovie } from '../actions/movieActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom'; // Import useParams
+import { useState } from 'react';
+
 
 const MovieDetail = () => {
   const dispatch = useDispatch();
-  const { movieId } = useParams();
-
+  const { movieId } = useParams(); // Get movieId from URL parameters
   const selectedMovie = useSelector(state => state.movie.selectedMovie);
-  const loading = useSelector(state => state.movie.loading);
-  const error = useSelector(state => state.movie.error);
+  const loading = useSelector(state => state.movie.loading); // Assuming you have a loading state in your reducer
+  const error = useSelector(state => state.movie.error); // Assuming you have an error state in your reducer
 
-  const [rating, setRating] = useState(5);
-  const [review, setReview] = useState("");
 
   useEffect(() => {
     dispatch(fetchMovie(movieId));
   }, [dispatch, movieId]);
 
-  // SUBMIT REVIEW FUNCTION
-  const submitReview = async () => {
-    try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/reviews`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: localStorage.getItem("token")
-        },
-        body: JSON.stringify({
-          movieId: selectedMovie._id,
-          rating,
-          review
-        })
-      });
-
-      if (res.ok) {
-        dispatch(fetchMovie(movieId));
-        setReview("");
-        setRating(5);
-      }
-    } catch (err) {
-      console.error(err);
+  const DetailInfo = () => {
+    if (loading) {
+      return <div>Loading....</div>;
     }
-  };
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-  if (!selectedMovie) return <div>No movie data</div>;
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
 
-  return (
-    <Card className="bg-dark text-white p-4 rounded">
-      <Card.Header>Movie Detail</Card.Header>
+    if (!selectedMovie) {
+      return <div>No movie data available.</div>;
+    }
 
-      <Card.Body>
-        <Image src={selectedMovie.imageUrl} thumbnail />
-      </Card.Body>
+    return (
+  <Card className="bg-dark text-dark p-4 rounded">
+    <Card.Header>Movie Detail</Card.Header>
 
-      <ListGroup>
-        <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+    <Card.Body>
+      <Image 
+        className="image" 
+        src={selectedMovie.imageUrl} 
+        thumbnail 
+      />
+    </Card.Body>
 
-        <ListGroupItem>
-          {selectedMovie.actors?.map((actor, i) => (
-            <p key={i}>
-              <b>{actor.actorName}</b> {actor.characterName}
-            </p>
-          ))}
-        </ListGroupItem>
+    <ListGroup>
+      <ListGroupItem>{selectedMovie.title}</ListGroupItem>
 
-        <ListGroupItem>
-          <h4>
-            <BsStarFill /> {selectedMovie.avgRating?.toFixed(1) || 0}
-          </h4>
-        </ListGroupItem>
-      </ListGroup>
-
-      {/* ADD REVIEW FORM */}
-      <Card.Body className="bg-light text-dark mt-3">
+      // Add review
+      <ListGroupItem>
+        {selectedMovie.actors?.map((actor, i) => (
+          <p key={i}>
+            <b>{actor.actorName}</b> {actor.characterName}
+          </p>
+        ))}
+      </ListGroupItem>
+        <Card.Body className="bg-light text-dark mt-3">
         <h5>Add Review</h5>
 
         <input
@@ -101,16 +79,52 @@ const MovieDetail = () => {
         <button onClick={submitReview}>Submit Review</button>
       </Card.Body>
 
-      {/* REVIEW LIST */}
-      <Card.Body className="bg-white text-dark">
-        {selectedMovie.movieReviews?.map((r, i) => (
-          <p key={i}>
-            <b>{r.username}</b> {r.review} <BsStarFill /> {r.rating}
-          </p>
-        ))}
+      <ListGroupItem>
+        <h4>
+          <BsStarFill /> {selectedMovie.avgRating?.toFixed(1) || 0}
+        </h4>
+      </ListGroupItem>
+    </ListGroup>
+    
+	{/* ADD REVIEW FORM */}
+      <Card.Body className="bg-light text-dark mt-3">
+        <h5>Add Review</h5>
+
+        <input
+          type="number"
+          min="1"
+          max="5"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <br />
+
+        <textarea
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+          placeholder="Write a review..."
+        />
+
+        <br />
+
+        <button onClick={submitReview}>Submit Review</button>
       </Card.Body>
-    </Card>
-  );
+    // Show review list
+    <Card.Body className="card-body bg-white">
+      {selectedMovie.movieReviews?.map((review, i) => (
+        <p key={i}>
+          <b>{review.username}</b> {review.review} <BsStarFill /> {review.rating}
+        </p>
+      ))}
+    </Card.Body>
+  </Card>
+  
+  
+);
+  };
+  // return <DetailInfo />;
 };
+
 
 export default MovieDetail;
